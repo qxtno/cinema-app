fe15bab3d0603810dfdb6f45d51c959d62713cb3
karol.sledz <qxtno@outlook.com>
2022-06-08 08:04:45 +0200

    Add entities

--- src/main/java/com/example/cinemaapp/cinema/Cinema.java
+++ src/main/java/com/example/cinemaapp/cinema/Cinema.java
@@ -1,15 +1,37 @@
 package com.example.cinemaapp.cinema;
 
-
 import com.example.cinemaapp.room.Room;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
 
+import javax.persistence.*;
 import java.util.List;
 import java.util.UUID;
 
-
+@Entity
+@Table(name = "cinema")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
 public class Cinema {
 
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
     private UUID id;
     private String city;
+
+    @JsonIgnore
+    @Transient
     private List<Room> rooms;
+
+    public Cinema(String city) {
+        this.city = city;
+    }
 }
--- src/main/java/com/example/cinemaapp/screening/Screening.java
+++ src/main/java/com/example/cinemaapp/screening/Screening.java
@@ -1,12 +1,31 @@
 
 package com.example.cinemaapp.screening;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.time.ZonedDateTime;
 import java.util.UUID;
 
-
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Entity
+@Table(name = "screening")
 public class Screening {
 
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
     private UUID id;
     private UUID cinemaId;
     private UUID roomId;
